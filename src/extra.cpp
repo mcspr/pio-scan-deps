fe0e548fb2042e7573c1b646389641e7df53ebcb
Maxim Prokhorov <prokhorov.max@outlook.com>
2022-05-01 15:08:22 +0300

    test

---
+++ src/extra.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+#if THIS_IS_A_TEST
+#include <testlib.h>
+
+void test() {
+}
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "extra.h"
+
+void setup() {
+#if THIS_IS_A_TEST
+    test();
+#endif
+}
+
+void loop() {
+}
+
